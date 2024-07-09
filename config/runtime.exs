59b8ae081c8039aed1796f492ad556645e748199
Rodrigo Vasconcelos <digocelos@gmail.com>
2024-07-09 13:55:10 -0300

    Update gigalixir config

--- config/runtime.exs
+++ config/runtime.exs
@@ -48,7 +48,7 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("APP_NAME") || "example.com"
+  host = System.get_env("APP_NAME") <> ".gigalixirapp.com" || "example.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :toy_app, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
@@ -56,11 +56,6 @@ if config_env() == :prod do
   config :toy_app, ToyAppWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
     http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
     ],
     secret_key_base: secret_key_base
